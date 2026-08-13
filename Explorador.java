b74521d43368ffe3d7d0879b73101c7899135105
sesa-xp <sesa.dev@hotmail.com>
2026-08-12 22:44:09 -0300

    Implement Runnable in Explorador class

--- Explorador.java
+++ Explorador.java
@@ -2,7 +2,20 @@
  * Classe abstrata que representa um explorador na Caça ao Tesouro Paralela.
  * Define a estrutura básica para diferentes tipos de exploradores.
  */
-public abstract class Explorador {
+public abstract class Explorador implements Runnable  {
+    protected String nome;
+    protected String tipo;
+    protected int prioridade;
+    protected String tarefa;
+
+    public Explorador( String nome, String tipo, int prioridade, String tarefa) {
+        this.nome= nome;
+        this.tipo= tipo;  
+        this.prioridade= prioridade;
+        this.tarefa= tarefa; 
+
+    }
+
     
     // * Construtor que inicializa todos os atributos do explorador.
     
@@ -14,6 +27,18 @@ public abstract class Explorador {
      * @throws TarefaInvalidaException Se a tarefa for inválida
      */
     public abstract void executarTarefa() throws TarefaInvalidaException;
+
+    @override
+    public void run() {
+        try {
+            executarTarefa();
+        } catch (TarefaInvalidaException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+
+    }
+
+}
     
     /**
      * Exibe o status completo do explorador com formatação clara.
